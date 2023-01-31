ddb5079414f277f27f2bca76b659eb9e0d7f088a
Sergey Martinov <martinovserg@gmail.com>
2023-01-31 16:29:23 +0700

    тут мы добавляем метод вычисления макс. продажи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] sales = {150, 300, 1000};
+        SalesManager sergey = new SalesManager(sales);
+        System.out.println(sergey.max());
     }
 }
